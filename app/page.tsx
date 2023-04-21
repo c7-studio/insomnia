5c35cf62361161f6c3fb7ed7b6115975293c90b5
c7-studio <c7studio.www@gmail.com>
2023-04-20 22:04:03 -0300

    bcmp

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Link from "next/link";
 import Marquee from "react-fast-marquee";
 
 
@@ -9,16 +10,21 @@ export default function Home() {
 
   return (
     <div className="h-screen w-full bg-black">
-          <Marquee gradient={false} className="bg-gray-500 text-white">
-            {marquee}
-          </Marquee>
-      <div className="flex justify-center items-center py-4 pt-10 flex-col">
+      <Marquee gradient={false} className="bg-gray-500 text-white">
+        {marquee}
+      </Marquee>
+      <div className="flex justify-center items-center py-4 pt-7 flex-col">
         <h1 className="text-6xl text-white uppercase">Insomnia</h1>
         <p className="text-white">Nuevo lanzamiento jueves 20 de abril</p>
         <p className="text-white text-left">Plantasia: visual Contragolpe: sonidos</p>
-        <div className="py-1">
-          <div className="flex justify-center py-10">
-            <iframe width="1400" height="500" src="https://www.youtube.com/embed/63gA8B3FwWo" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+        <p className="text-white text-left">Tambien DISPONIBLE en 
+          <span className="hover:text-yellow">
+            <Link href='https://insomnia111.bandcamp.com/track/insomnia'> Bandcamp</Link>
+          </span>
+        </p>
+        <div className="">
+          <div className="flex justify-center py-6">
+            <iframe width="1400" height="500" src="https://www.youtube.com/embed/63gA8B3FwWo" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
           </div>
         </div>
       </div>
